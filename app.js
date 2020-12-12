5886220919e2025cddbb78a68bf6a0f34a6eac3f
Sanuel Bishara <sambishara99@gmail.com>
2020-12-12 02:03:39 -0500

    Started building http endpoints

--- app.js
+++ app.js
@@ -11,6 +11,40 @@ mongoose.connect('mongodb://localhost:27017/booksDB', {
     useUnifiedTopology: true
 });
 
+const bookSchema = {
+    title: String,
+    author: String,
+    imgURL: String,
+    review: String
+}
+const Book = mongoose.model("Book", bookSchema);
+
+app.route('/books')
+.get((req, res) => {
+    Book.find({}, (e, foundItems) => {
+        if (e) {
+            res.send(e);
+        } else {
+            res.send(foundItems);
+        }
+    });
+})
+.post((req, res) => {
+    const newBook = new  Book({
+        title: req.body.title,
+        author: req.body.author,
+        imgURL: req.body.imgURL,
+        review: req.body.review
+    });
+    newBook.save((e) => {
+        if (e) {
+            res.send(e);
+        } else {
+            res.send('Document saved successfully!')
+        }
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server running successfully on port 3000')
 })
